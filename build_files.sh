22b1a7e313c476b8a190435e9e2e1e996f65873d
unknown <riswanbasha26@gmail.com>
2024-01-11 00:51:48 +0100

    Update 4

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,10 @@
+#!/bin/bash
+
+# Activate the virtual environment
+source venv/bin/activate
+
+# Install dependencies from requirements.txt
 pip install -r requirements.txt
+
+# Run collectstatic command
 python manage.py collectstatic
