0ff3734e8114ceabf5aef080b7150e4294607ee0
unknown <riswanbasha26@gmail.com>
2024-01-11 01:08:17 +0100

    Update 11

--- build_files.sh
+++ build_files.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Activate the virtual environment
-source ./venv/Scripts/activate
+venv/Scripts/activate
 
 # Install dependencies from requirements.txt
 pip install -r requirements.txt
