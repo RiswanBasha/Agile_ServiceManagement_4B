dad835b79941f17736f4cb2a44f40ae4004bbeca
unknown <riswanbasha26@gmail.com>
2024-01-11 11:09:32 +0100

    Update 19

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Redirect output to a log file
+exec > >(tee /tmp/build_files.log) 2>&1
+
 # Activate the virtual environment
 source venv/bin/activate   # Adjust the path based on your project structure
 
@@ -8,3 +11,4 @@ pip install -r requirements.txt
 
 # Run collectstatic command
 python manage.py collectstatic --noinput
+
