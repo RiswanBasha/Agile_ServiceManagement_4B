30c2473e833b0a7d1d1748bd04c760ecf34cb94f
unknown <riswanbasha26@gmail.com>
2024-01-11 11:06:06 +0100

    Update 18

--- build_files.sh
+++ build_files.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Activate the virtual environment
-venv/Scripts/activate
+source venv/bin/activate   # Adjust the path based on your project structure
 
 # Install dependencies from requirements.txt
 pip install -r requirements.txt
 
 # Run collectstatic command
-python manage.py collectstatic
+python manage.py collectstatic --noinput
