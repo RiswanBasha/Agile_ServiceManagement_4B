2bb56a62b5d73ccf615746d2ec1c607f7c1c08d3
unknown <riswanbasha26@gmail.com>
2024-01-11 01:35:55 +0100

    Update 15

--- manage.py
+++ manage.py
@@ -3,8 +3,9 @@
 import os
 import sys
 
+
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'servicemanagement.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vehicleservicemanagement.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -12,8 +13,7 @@ def main():
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
-        ) 
-
+        ) from exc
     execute_from_command_line(sys.argv)
 
 
